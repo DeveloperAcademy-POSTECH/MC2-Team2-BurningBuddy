42b98bc13658beb6d15ae64a955a256a6b6a2dc4
dhk767 <dhk767@gmail.com>
2023-05-03 00:50:32 +0900

    [feat] #2 - CharacterSettingView 구현 완료

--- BurningBuddy/View/CharacterSettingView.swift
+++ BurningBuddy/View/CharacterSettingView.swift
@@ -9,8 +9,41 @@ import Foundation
 import SwiftUI
 
 struct CharacterSettingView: View {
+    @ObservedObject var characterName = TextLimiter(limit: 8)
+    
     var body: some View {
-        Text("캐릭터 생성뷰")
+        VStack {
+            Text("캐릭터 이름을\n설정해봐요!")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(EdgeInsets(top: 50, leading: 30, bottom: 0, trailing: 0))
+                .foregroundColor(.white)
+                .font(.custom("본고딕-Bold", size: 30))
+            Spacer()
+            Spacer()
+            TextField("", text: $characterName.value, prompt: Text("캐릭터 이름은 한글 2~8자로 설정할 수 있어요!")
+                .foregroundColor(.white))
+            .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
+            .foregroundColor(.white)
+            Divider()
+                .overlay(Color.white)
+                .padding(EdgeInsets(top: 0, leading: 30, bottom: 0, trailing: 30))
+            
+            Image("cookie")
+                .scaledToFill()
+                .frame(width: 400, height: 400)
+            
+            Button("캐릭터 만들기", action: {
+                // saveCharacterName()
+                
+            })
+            .buttonStyle(NextButtonStyle(colorRed: 255, colorGreen: 45, colorBlue: 85, fontSize: 17))
+            Spacer()
+        }
+        .background(.black)
+    }
+    
+    func saveCharacterName() {
+        // UserDefault에 캐릭터 이름 저장
     }
 }
 
