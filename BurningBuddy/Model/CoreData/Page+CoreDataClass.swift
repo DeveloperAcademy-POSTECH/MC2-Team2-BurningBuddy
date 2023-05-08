931d31f92e3136ab17cceab263099b14ae106b07
kpk0616 <kpk0616@naver.com>
2023-05-08 19:54:46 +0900

    [Add] #11 - Page 코어데이터 추가

---
+++ BurningBuddy/Model/CoreData/Page+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Page+CoreDataClass.swift
+//  BurningBuddy
+//
+//  Created by 박의서 on 2023/05/08.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Page)
+public class Page: NSManagedObject {
+
+}
---
+++ BurningBuddy/Model/CoreData/Page+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Page+CoreDataProperties.swift
+//  BurningBuddy
+//
+//  Created by 박의서 on 2023/05/08.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Page {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Page> {
+        return NSFetchRequest<Page>(entityName: "Page")
+    }
+
+    @NSManaged public var pageNum: Int16
+
+}
+
+extension Page : Identifiable {
+
+}
