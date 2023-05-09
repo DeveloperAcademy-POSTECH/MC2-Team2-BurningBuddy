72c27086d829f14189c6b432e66b0f552cfda492
kpk0616 <kpk0616@naver.com>
2023-05-09 19:11:23 +0900

    [Feat] #20 - SearchPartner 뷰 NISessionManager 객체 생성

--- BurningBuddy/View/Partner/SearchPartnerView.swift
+++ BurningBuddy/View/Partner/SearchPartnerView.swift
@@ -15,22 +15,16 @@ import SwiftUI
  */
 struct SearchPartnerView: View {
     @EnvironmentObject var settings: UserSettings
-    @State var isSearchedPartner: Bool = true
-    @State var notFoundPartner: Bool = false
-    @State var partnerData: String = "상대방 닉네임" // 데이터 타입 지정 필요
+    @State var isSearchedPartner: Bool = false // 화면 전환용
+    @State var notFoundPartner: Bool = false // 모달용
+    @State var partnerData: String = "상대방 닉네임" // TODO: - 데이터 타입 지정 필요
+    // niObject 생성
+    @StateObject var niObject = NISessionManager()
     var body: some View {
         
         VStack {
-            if isSearchedPartner {
-                Text("내 주변 파트너를 \n발견했어요")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .foregroundColor(.white)
-                    .font(.system(size: 30, weight: .bold, design: .default))
-                Text("연결할 파트너가 맞나요?")
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .foregroundColor(.white)
-                    .padding(EdgeInsets(top: 1, leading: 0, bottom: 90, trailing: 0))
-            } else {
+          switch(niObject.findingPartnerState) {
+          case .ready, .finding:
                 // 파트너를 찾을때 NI를 가져오면 됩니당 ~!!
                 Text("내 파트너를 \n찾는 중이에요")
                     .frame(maxWidth: .infinity, alignment: .leading)
@@ -40,8 +34,17 @@ struct SearchPartnerView: View {
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .foregroundColor(.white)
                     .padding(EdgeInsets(top: 1, leading: 0, bottom: 90, trailing: 0))
-               
-            }
+
+          case .found:
+                Text("내 주변 파트너를 \n발견했어요")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.white)
+                    .font(.system(size: 30, weight: .bold, design: .default))
+                Text("연결할 파트너가 맞나요?")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(.white)
+                    .padding(EdgeInsets(top: 1, leading: 0, bottom: 90, trailing: 0))
+          }
             ZStack {
                 Circle()
                     .foregroundColor(Color(red: 44/255, green: 44/255, blue: 46/255))
@@ -53,35 +56,35 @@ struct SearchPartnerView: View {
                     .foregroundColor(Color(red: 124/255, green: 124/255, blue:129/255, opacity: 0.8))
                     .padding(EdgeInsets(top: 54, leading: 54, bottom: 54, trailing: 54))
                 VStack {
-                    if isSearchedPartner {
-                        Image("Image")
-                        Text(partnerData)
-                        
-                    } else {
-                        Text("이미지가 들어갈 것이다.")
-                        Text("검색중...")
-                        
-                    }
+                  switch(niObject.findingPartnerState) {
+                  case .found:
+                    Image("Image")
+                    Text(partnerData)
+                  case .ready, .finding:
+                    Text("이미지가 들어갈 것이다.")
+                    Text("검색중...")
+                  }
                 }
                 .padding(EdgeInsets(top: 106, leading: 106, bottom: 106, trailing: 106))
-                
             }
             Spacer()
-            if isSearchedPartner {
-                HStack {
-                    Button("다시 연결할래요", action: {
-                        
-                    })
-                    .buttonStyle(GrayButtonStyle())
+          switch(niObject.findingPartnerState) {
+          case .found:
+            HStack {
+                Button("다시 연결할래요", action: {
                     
-                    Button("연결하기", action: {
-                        
-                    })
-                    .buttonStyle(RedButtonStyle())
-                }
-            } else {
-
+                })
+                .buttonStyle(GrayButtonStyle())
+                
+                Button("연결하기", action: {
+                    
+                })
+                .buttonStyle(RedButtonStyle())
             }
+          case .ready, .finding:
+            Text("")
+          }
+            
         }
         .padding(EdgeInsets(top: 20, leading: 25, bottom: 10, trailing: 25))
         .background(Color(red: 30/255, green: 28/255, blue: 29/255)) // 고급진 까만것이 필요할 듯
