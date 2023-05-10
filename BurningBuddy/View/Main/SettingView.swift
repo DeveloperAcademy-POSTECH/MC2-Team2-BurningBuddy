b02c88a2169b93479f7524410b1f6547e91bb116
Hanyeonggyun <hyg100@naver.com>
2023-05-10 15:19:39 +0900

    [Feat] #22 - Setting View Completed

--- BurningBuddy/View/Main/SettingView.swift
+++ BurningBuddy/View/Main/SettingView.swift
@@ -12,24 +12,68 @@ import SwiftUI
  단, update 할 수 있는 변수가,,, 칼로리가 있기 때문에, 그것만 업데이트 시키는 코드를 만들어서 넣어주어야 한다.
  */
 struct SettingView: View {
+    @EnvironmentObject var settings: UserSettings
+    
     var body: some View {
-        ZStack{
-            VStack{
-                Text("내 정보")
-                    .offset(y: 10)
-               Rectangle()
-                    .frame(height: 74)
-                    .padding(EdgeInsets(top: 63, leading: 15, bottom: 0, trailing: 15))
+    
+            List {
+                Section {
+                    HStack{
+                        
+                        Circle()
+                            .frame(width: 57, height: 57)
+                        VStack (alignment: .leading){
+                            
+                            Text(settings.nickName)
+                                .foregroundColor(.white)
+                                .font(.system(size: 21, weight: .bold))
+                            Text(settings.characterName)
+                            
+                        }.padding(12)
+                        
+                        
+                    }
+                    
+                }
+                Section {
+                    NavigationLink{
+                        CalorieSettingView()
+                    } label: {
+                        HStack{
+                            Image(systemName: "checkmark.seal.fill")
+                                .background(Color(red: 255/255, green: 0/255, blue: 82/255))
+                            Text("목표 칼로리")
+                            
+                        }
+                        
+                    }
+                    HStack{
+                        Image(systemName: "checkmark.seal.fill")
+                            .background(Color(red: 255/255, green: 0/255, blue: 82/255))
+                        Text("모은 핑크 덤벨 개수")
+                        Spacer()
+                        Text(String(settings.totalDumbbell)+"개")
+                            .foregroundColor(.gray)
+                    }
+                    
+                }
+                
             }
-        }
-     
+            .navigationTitle("내 정보")
+            .navigationBarTitleDisplayMode(.inline)
         
+  
+        
+
     }
 }
-
+   
+   
+  
 
 struct SettingView_Previews: PreviewProvider {
     static var previews: some View {
         SettingView()
+            .environmentObject(UserSettings())
     }
 }
