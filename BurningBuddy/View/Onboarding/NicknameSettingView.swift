32d274d045949907c1c961acb796ea8b08a62f70
dhk767 <dhk767@gmail.com>
2023-05-10 11:48:41 +0900

    [feat] #25 - 닉네임세팅뷰 color 코드 정리, 닉네임 미설정 시 화면 전환 제어 구현 완료

--- BurningBuddy/View/Onboarding/NicknameSettingView.swift
+++ BurningBuddy/View/Onboarding/NicknameSettingView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct NicknameSettingView: View {
     @ObservedObject var nicknameLimiter = TextLimiter(limit: 8)
     @EnvironmentObject var settings: UserSettings
+    @State private var isInputText: Bool = false
     @State private var firstSliderDrag: Bool = false
     @State private var sliderValue: Double = 200
     @State private var sliderMessage: String = "목표 칼로리를 설정해주세요!"
@@ -19,22 +20,28 @@ struct NicknameSettingView: View {
         VStack {
             Text("닉네임을\n입력해주세요!")
                 .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundColor(.white)
+                .foregroundColor(Color.mainTextColor)
                 .font(.system(size: 30, weight: .bold))
             Text("나중에 연결된 파트너에게 보여져요")
                 .padding(EdgeInsets(top: 1, leading: 0, bottom: 0, trailing: 0))
                 .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundColor(Color(red: 1, green: 1, blue: 1, opacity: 0.65))
+                .foregroundColor(Color.subTextColor)
                 .font(.system(size: 17, weight: .medium))
                 .lineSpacing(TextUtil().calculateLineSpacing(17, 143.5))
             Spacer()
             Spacer()
             TextField("", text: $nicknameLimiter.value, prompt: Text("닉네임은 한글 2~8자로 설정할 수 있어요!")
-                .foregroundColor(.white))
-            .foregroundColor(.white)
+                .foregroundColor(Color.mainTextColor))
+            .foregroundColor(Color.mainTextColor)
             Divider()
                 .overlay(Color.white)
-            
+            if isInputText {
+                Text("닉네임이 입력되지 않았어요!")
+                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 0, trailing: 0))
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    .foregroundColor(Color.primary)
+                    .font(.system(size: 17, weight: .bold))
+            }
             Spacer()
             Spacer()
             Button("다음", action: {
@@ -43,13 +50,18 @@ struct NicknameSettingView: View {
             .buttonStyle(RedButtonStyle())
         }
         .padding(EdgeInsets(top: 30, leading: 30, bottom: 30, trailing: 30))
-        .background(Color(red: 30/255, green: 28/255, blue: 29/255)) // 고급진 까만것이 필요할 듯
+        .background(Color.backgroundColor) // 고급진 까만것이 필요할 듯
     }
     
     private func saveNickname() {
         settings.nickName = nicknameLimiter.value
-        withAnimation(.easeInOut(duration: 0.5)){
-            settings.pageNum += 1
+        if settings.nickName.count == 0 {
+            self.isInputText = true
+        } else {
+            self.isInputText = false
+            withAnimation(.easeInOut(duration: 0.5)){
+                settings.pageNum += 1
+            }
         }
     }
 }
