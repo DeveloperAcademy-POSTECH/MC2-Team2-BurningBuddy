071e0646cfb694658a5774b340ada6e05d9838ae
Hanyeonggyun <hyg100@naver.com>
2023-05-10 09:08:20 +0900

    [feat] #12 - 내정보 화면 구현 중

--- BurningBuddy/View/Main/SettingView.swift
+++ BurningBuddy/View/Main/SettingView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct SettingView: View {
     var body: some View {
-        Text("세팅화면")
+        ZStack{
+            Text("내 정보")
+                .font(.system(size: 17, weight: .bold, design: .default))
+                .padding(.bottom, 800)
+            Rectangle()
+                .frame(width: 344.5, height: 74.54)
+                .foregroundColor(Color(red: 2.0, green: 2.0, blue: 5.0))
+                .cornerRadius(20)
+        }
     }
 }
 
