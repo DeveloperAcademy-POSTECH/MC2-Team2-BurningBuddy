9b463817e771b2cee47fd90fa44aad4ef6e34b9a
Hanyeonggyun <hyg100@naver.com>
2023-05-14 23:06:41 +0900

    [Feat] #52 - 세팅뷰 캐릭터 레벨별 변화

--- BurningBuddy/View/Main/SettingView.swift
+++ BurningBuddy/View/Main/SettingView.swift
@@ -18,8 +18,42 @@ struct SettingView: View {
         List {
           Section {
             HStack{
-              Circle()
-                .frame(width: 57, height: 57)
+                switch settings.level {
+                case 1:
+                    Image("Bunny_Level1_반측면")
+                          .resizable()
+                          .frame(width: 94, height: 79)
+                case 2:
+                    Image("Bunny_Level2_반측면")
+                          .resizable()
+                          .frame(width: 94, height: 79)
+                case 3:
+                    Image("Bunny_Level3_반측면")
+                          .resizable()
+                          .frame(width: 94, height: 79)
+                case 4:
+                    Image("Bunny_Level4_반측면")
+                          .resizable()
+                          .frame(width: 94, height: 79)
+                case 5:
+                    Image("Bunny_Level5_반측면")
+                          .resizable()
+                          .frame(width: 94, height: 79)
+                case 6:
+                    Image("Bunny_Level6_반측면")
+                          .resizable()
+                          .frame(width: 94, height: 79)
+                case 7:
+                    Image("Bunny_Level7_반측면")
+                          .resizable()
+                          .frame(width: 94, height: 79)
+                default:
+                    Text("레벨이 7을 초과")
+                }
+            
+              
+               
+            
               VStack (alignment: .leading){
                 Text(settings.nickName)
                   .foregroundColor(.white)
