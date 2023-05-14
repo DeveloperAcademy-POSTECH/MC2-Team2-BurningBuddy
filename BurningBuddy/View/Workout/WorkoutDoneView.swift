de4736452580aa00734a3c406a59988f01ad873c
kpk0616 <kpk0616@naver.com>
2023-05-14 18:52:09 +0900

    [Fix] #36 - WorkoutDoneView에서 isBumped된 후, 상대목표 확인 후 모달 띄우기

--- BurningBuddy/View/Workout/WorkoutDoneView.swift
+++ BurningBuddy/View/Workout/WorkoutDoneView.swift
@@ -17,107 +17,124 @@ import SwiftUI
  
  */
 struct WorkoutDoneView: View {
-    @EnvironmentObject var settings: UserSettings
-    @State var isNotDoneWorkoutPopup = false
-    @State private var isSuccessNext: Bool = false
-    @Binding var mainViewNavLinkActive: Bool
-    
-    @StateObject private var niObject = NISessionManager()
-    @State private var isLaunched = true
-    @State var isLocalNetworkPermissionDenied = false
-    @State private var checkPartner: Bool = false
-    @State var isDataReceived: Bool = false
-    private let localNetAuth = LocalNetworkAuthorization()
-    
-    var body: some View {
-        VStack {
-            Text("\(settings.nickName)님")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundColor(.white)
-                .font(.system(size: 21, weight: .bold))
-            Text("수고하셨어요!")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundColor(.white)
-                .font(.system(size: 28, weight: .bold))
-            Text("애플워치 운동기록을 종료하신 후\n오늘 함께 운동한 파트너와\n디바이스를 접촉해주세요!")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .padding(EdgeInsets(top: 1, leading: 0, bottom: 0, trailing: 0))
-                .font(.system(size: 17, weight: .regular, design: .default))
-                .lineSpacing(TextUtil().calculateLineSpacing(17, 143.5))
-            
-            Spacer()
-            ZStack {
-                Image(systemName: "hands.sparkles.fill")
-                    .resizable()
-                    .frame(width: 189, height: 189)
-                    .foregroundColor(Color.bunnyColor)
-            }
-            Spacer()
-            NavigationLink(isActive: $isSuccessNext, destination: {
-                if settings.isDoneTogetherWorkout {
-                    WorkoutSuccessView(mainViewNavLinkActive: $mainViewNavLinkActive)
-                } else {
-                    WorkoutFailView(mainViewNavLinkActive: $mainViewNavLinkActive)
-                }
-                
-            }, label: {
-                Button("목표달성 확인하기") {
-                    print("Navi link 안")
-                    
-                    // TODO: DataReeiveView를 만들어준다 -> 모달 뷰(DataReeiveView) 띄우기 -> 상대방 데이터 통신 -> 모달 뷰 해제
-                    
-                    self.checkPartner = true // 모달뷰 띄우기
-                    if niObject.isBumped {
-                        self.checkPartner = false
-                        if !niObject.isDoneTargetCalories || UserDefaults.standard.bool(forKey: "isDoneWorkout") { // isDoneWorkout: 목표달성 여부 체크 부울값
-                            self.isNotDoneWorkoutPopup = true
-                        } else {
-                            self.isSuccessNext = true
-                        }
-                    }
-                    
-                    
-                    // print("niObject에 저장된 상대방 id", niObject.bumpedID?.uuidString ?? "값 없음")
-                    // print("userDefalut에 저장된 상대방 id", UserDefaults.standard.string(forKey: "partnerID") ?? "값 없음")
-                    // 다음 뷰로 넘어가는 변수
-                }
-                .fullScreenCover(isPresented: self.$checkPartner, content: {
-                    // 상대방이 운동을 달성했는지 확인하는 부분
-                    DataReceiveView(niObject: niObject, isDataReceived: $isSuccessNext, checkPartner: $checkPartner) // TODO: - niObject가 같은 변수를 공유하는지 확인하기 - 잘 돌아간다고 치고
-                        .environmentObject(settings)
-                })
-                
-                .buttonStyle(RedButtonStyle())
-            })
+  @EnvironmentObject var settings: UserSettings
+  @State var isNotDoneWorkoutPopup = false
+  @State private var isSuccessNext: Bool = false
+  @Binding var mainViewNavLinkActive: Bool
+  
+  @StateObject private var niObject = NISessionManager()
+  @State private var isLaunched = true
+  @State var isLocalNetworkPermissionDenied = false
+  @State private var checkPartner: Bool = false
+  @State var isDataReceived: Bool = false
+  private let localNetAuth = LocalNetworkAuthorization()
+  
+  var body: some View {
+      VStack {
+        switch(niObject.findingPartnerState) {
+        case .ready:
+          Text("ready")
+            .foregroundColor(Color.mainTextColor)
+        case .finding:
+          Text("finding")
+            .foregroundColor(Color.mainTextColor)
+        case .found:
+          Text("found")
+            .foregroundColor(Color.mainTextColor)
         }
+        switch(niObject.isBumped) {
+        case true:
+          Text("isBumped: true")
+        case false:
+          Text("isBumped: false")
+        }
+        Text("\(settings.nickName)님")
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .foregroundColor(.white)
+          .font(.system(size: 21, weight: .bold))
+        Text("수고하셨어요!")
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .foregroundColor(.white)
+          .font(.system(size: 28, weight: .bold))
+        Text("애플워치 운동기록을 종료하신 후\n오늘 함께 운동한 파트너와\n디바이스를 접촉해주세요!")
+          .frame(maxWidth: .infinity, alignment: .leading)
+          .padding(EdgeInsets(top: 1, leading: 0, bottom: 0, trailing: 0))
+          .font(.system(size: 17, weight: .regular, design: .default))
+          .lineSpacing(TextUtil().calculateLineSpacing(17, 143.5))
         
-        .padding(EdgeInsets(top: 50, leading: 30, bottom: 15, trailing: 30)) // 전체 아웃라인
-        .background(Color(red: 30/255, green: 28/255, blue: 29/255))
-        .sheet(isPresented: self.$isNotDoneWorkoutPopup) {
-            if #available(iOS 16.0, *) {
-                MissionResultModalView(title: "파트너가 아직 운동 중이에요!", article: "운동을 마칠 때까지 응원해주세요!", leftButtonName: "알겠어요", rightButtonName: "그만할래요", wantQuitWorkout: $isSuccessNext )
-                    .presentationDetents([.fraction(0.4)])
-                    .background(Color(red: 30/255, green: 28/255, blue: 29/255))
-                    .environmentObject(settings)
-                //  TODO: - 수정
-            }
+        Spacer()
+        ZStack {
+          Image(systemName: "hands.sparkles.fill")
+            .resizable()
+            .frame(width: 189, height: 189)
+            .foregroundColor(Color.bunnyColor)
         }
-        .navigationBarHidden(true)
-    }
-    
-    
-    
-    
-    //  private func getDestination() -> some View {
-    //    startNI()
-    //    // peer의 uuid가 저장되어있는 settings.partnerID와 일치하는지 확인
-    //    if settings.isDoneTogetherWorkout {
-    //      return AnyView(WorkoutSuccessView(mainViewNavLinkActive: $mainViewNavLinkActive))
-    //    }
-    //    else {
-    //      return AnyView(WorkoutFailView(mainViewNavLinkActive: $mainViewNavLinkActive))
-    //    }
-    //  }
+        Spacer()
+        switch(niObject.isBumped) {
+        case true:
+          NavigationLink(isActive: $isSuccessNext, destination: {
+            if settings.isDoneTogetherWorkout {
+              WorkoutSuccessView(mainViewNavLinkActive: $mainViewNavLinkActive)
+            } else {
+              WorkoutFailView(mainViewNavLinkActive: $mainViewNavLinkActive)
+            }
+            
+          }, label: {
+            Button("목표달성 확인하기") {
+              if (niObject.bumpedIsDoneTargetCalories) { // 상대 목표 달성 True
+                isNotDoneWorkoutPopup = false
+              } else {
+                isNotDoneWorkoutPopup = true
+              }
+            }
+            .buttonStyle(RedButtonStyle())
+          })
+        case false:
+          NavigationLink(isActive: $isSuccessNext, destination: {
+            if settings.isDoneTogetherWorkout {
+              WorkoutSuccessView(mainViewNavLinkActive: $mainViewNavLinkActive)
+            } else {
+              WorkoutFailView(mainViewNavLinkActive: $mainViewNavLinkActive)
+            }
+            
+          }, label: {
+            Button("접촉하기") {
+              // NIObject 통신 시작
+              switch niObject.findingPartnerState {
+              case .ready:
+                niObject.start()
+                niObject.findingPartnerState = .finding
+                if isLaunched {
+                  localNetAuth.requestAuthorization { auth in
+                    isLocalNetworkPermissionDenied = !auth
+                  }
+                  isLaunched = false
+                }
+              case .finding:
+                niObject.stop()
+                niObject.findingPartnerState = .ready
+              case .found:
+                niObject.stop()
+                niObject.findingPartnerState = .ready
+              }
+            }
+            .buttonStyle(GrayButtonStyle())
+          })
+        } // switch 끝
+      }
+      
+      .padding(EdgeInsets(top: 50, leading: 30, bottom: 15, trailing: 30)) // 전체 아웃라인
+      .background(Color(red: 30/255, green: 28/255, blue: 29/255))
+      .sheet(isPresented: self.$isNotDoneWorkoutPopup) {
+        if #available(iOS 16.0, *) {
+          MissionResultModalView(title: "파트너가 아직 운동 중이에요!", article: "운동을 마칠 때까지 응원해주세요!", leftButtonName: "알겠어요", rightButtonName: "그만할래요", wantQuitWorkout: $isSuccessNext )
+            .presentationDetents([.fraction(0.4)])
+            .background(Color(red: 30/255, green: 28/255, blue: 29/255))
+            .environmentObject(settings)
+        }
+      }
+      .navigationBarHidden(true)
+  } // body End
 }
 
 //
