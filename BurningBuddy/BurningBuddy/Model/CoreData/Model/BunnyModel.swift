b0868c451d8974cabdb73082a3f5b5009f3699f3
dhk767 <dhk767@gmail.com>
2023-08-17 20:03:43 +0900

    [add] #91 - UserModel 생성, 기본 메서드 fetchUserData() 구현 중

---
+++ BurningBuddy/BurningBuddy/Model/CoreData/Model/BunnyModel.swift
@@ -0,0 +1,8 @@
+//
+//  BunnyModel.swift
+//  BurningBuddy
+//
+//  Created by 김동현 on 2023/08/17.
+//
+
+import Foundation
---
+++ BurningBuddy/BurningBuddy/Model/CoreData/Model/UserModel.swift
@@ -0,0 +1,34 @@
+//
+//  UserModel.swift
+//  BurningBuddy
+//
+//  Created by 김동현 on 2023/08/17.
+//
+
+import Foundation
+
+class UserModel: ObservableObject {
+    final let entityName = "User"
+    static let shared = UserModel()
+    private let coreDataManager = CoreDataManager.shared
+    @Published var userName = ""
+    @Published var todayCalories = 0
+    @Published var todayWorkoutHours = 0
+    @Published var totalDumbbell = 0
+    @Published var goalCalories = 0
+    @Published var userID: UUID = UUID()
+    
+    private init() {
+        fetchUserData()
+    }
+    
+    func fetchUserData() {
+        let fetchResult = coreDataManager.fetch(entityName: entityName)
+        
+        guard let userData = fetchResult.first as? User else {
+            print("유저 데이터를 가져오는데 실패했습니다.(UserModel)")
+            return
+        }
+    }
+    
+}
