834b2c34a671f735869cbaa55bc2a6fbbda48fde
dhk767 <dhk767@gmail.com>
2023-08-18 11:59:13 +0900

    [refactor] #91 - BunnyModel 구성 완료

--- BurningBuddy/BurningBuddy/Model/CoreData/Model/BunnyModel.swift
+++ BurningBuddy/BurningBuddy/Model/CoreData/Model/BunnyModel.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+
+class BunnyModel: ObservableObject {
+    final let entityName = "Bunny"
+    static let shared = BunnyModel()
+    private let coreDataManager = CoreDataManager.shared
+    @Published var bunnyName = ""
+    @Published var bunnyLevel = 0
+    
+    private init() {
+        fetchUserData()
+    }
+    
+    func fetchUserData() {
+        let fetchResult = coreDataManager.fetch(entityName: entityName)
+        
+        guard let bunnyData = fetchResult.first as? Bunny else {
+            print("유저 데이터를 가져오는데 실패했습니다.(UserModel)")
+            return
+        }
+        
+        bunnyName = bunnyData.characterName ?? "버닝버디"
+        bunnyLevel = Int(bunnyData.level)
+    }
+    
+    func saveUserData() {
+        var bunnyData: Bunny?
+        
+        let fetchResult = coreDataManager.fetch(entityName: entityName)
+        if let existingUserData = fetchResult.first as? Bunny {
+            bunnyData = existingUserData
+        } else if let newBunnyData = coreDataManager.create(entityName: entityName, attributes: [:]) as? Bunny {
+            bunnyData = newBunnyData
+        }
+        
+        guard let bunny = bunnyData else {
+            print("유저 데이터 생성 또는 찾기에 실패했습니다.")
+            return
+        }
+        
+        bunnyName = bunny.characterName ?? "버닝버디"
+        bunnyLevel = Int(bunny.level)
+        coreDataManager.update(object: bunny)
+    }
+}
