d9dbb662f100f2972306aa30cd56b0ed04eb6dd5
dhk767 <dhk767@gmail.com>
2023-09-21 21:06:51 +0900

    [Add] SinglePlay 폴더에 빈 파일 생성

---
+++ BurningBuddy/BurningBuddy/SinglePlay/View/KaKaoTalkView.swift
@@ -0,0 +1,20 @@
+//
+//  KaKaoTalkView.swift
+//  BurningBuddy
+//
+//  Created by 김동현 on 2023/09/21.
+//
+
+import SwiftUI
+
+struct KaKaoTalkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KaKaoTalkView_Previews: PreviewProvider {
+    static var previews: some View {
+        KaKaoTalkView()
+    }
+}
